08692314f22fae8c2ff86911576a71c3f9eda905
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-03-30 23:13:31 +0530

    Create numeric.js

---
+++ numeric.js
@@ -0,0 +1,22 @@
+const score = 400
+console.log(score)
+
+const balance = new Number (100);
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(1)) // we got 100.0 one zero
+
+const othernumber= 123.8983
+console.log(othernumber.toPrecision(4))
+
+const hundered= 1000000;
+console.log(hundered.toLocaleString('en-IN')) // it will give indian rupee with coma
+
+// ----------------------math ------------------------//
+console.log(Math);
+console.log(Math.abs(-4))  // it will convert minus value to positive if i give positive  so  positive remain same
+console.log(Math.round(4.6)) // round value give 
+console.log(Math.min(12,22,51,24,90))
+console.log(Math.max(12,22,51,24,90))
+
