087c68ec1a875bfd4a7f0395c10e3e21a53fc7ef
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-04-03 23:15:41 +0530

    Create control flow.js

---
+++ control flow.js
@@ -0,0 +1,54 @@
+
+if(2<6){
+    // console.log("yes")
+}
+//  console.log("No")
+
+
+
+ //  or more method
+
+ const age = 67
+
+ if(age>70){
+    // console.log("age is greater then ")
+ } else{
+    // console.log("age is less than ")
+ }
+
+ // or more things
+
+  const score = 339
+  if(score>200){
+    const power= "fly"
+   //  console.log(`user of power: ${power}`)
+  }
+  //console.log(`user of power: ${power}`)  // it will give error power is not defined 
+  //becase its a not  scope of const if i give var its a globole so power so i can use out of scope this line
+
+
+
+  // more thing
+  const balance= 1290
+//   if(balance>560) console.log("is greater");
+
+  // more thing 
+
+  const userlogged= true
+  const debitcard= true 
+  if(userlogged && debitcard &&  2===2  ){      // we can check multiple conditon 
+    console.log("this is eligible for shoppping")
+  } else {
+      console.log("not eligible")
+  }
+
+  // or operstor use
+
+  const loggedgoogle= false
+  const loggedEmail =  true
+
+  if(loggedgoogle || loggedEmail ){
+     console.log("u can login both ")
+  }
+
+  
