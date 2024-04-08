4a02480d9f3e057d094aef7c1ac4dcebfcf31754
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-04-08 23:49:31 +0530

    Create function.js

---
+++ function.js
@@ -0,0 +1,46 @@
+
+// function mydetails (){
+//     console.log("Naseem choudhary")
+//     console.log("im 21 year old")
+//     console.log("pursuing b tech")
+//     console.log("computer science ")
+// }
+
+// mydetails()
+
+
+function sumoftwoNumbers(num1,num2){  // pass the input parameter
+    console.log(num1+num2)
+}
+
+// sumoftwoNumbers(10,11) // pass the argument
+
+// or we can do like it
+
+function sumoftwoNumbers(num1,num2){  
+    //  let result = num1+num2  
+    //  return result
+     return num1+num2  // we can do like tha also
+}
+
+
+const result = sumoftwoNumbers(20,10)  // we cam pass argument in like a variable
+// console.log("result is : ",result)
+
+
+
+// more thing for the function
+
+function calculatcartprice(...num1){
+
+    return num1
+}
+
+console.log(calculatcartprice(1200,100,10000,45,300))  // ... with dot we can find these all value whihc i passed in argumnet
+
+
+
+
+
+
+
