f6f32b323b65eb91096742ceef5bf46a01e5051c
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-04-11 23:50:12 +0530

    Create switch.js

---
+++ switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+// switch use like a key form
+const month =2  // we can here string like "jan" ;
+
+switch (month) {
+    case 1:  // here we can write "jan" 
+        console.log("january")
+        break;
+     case 2:
+        console.log("Febuary")
+        break;
+        case 3:
+            console.log("march")
+            break;
+        case 4:
+            console.log("april")
+    default:
+        console.log("nothing match so this is default case")
+        break;
+}
